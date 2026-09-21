4e1a7a002237a16b63ef197a5e2b35d1404c4500
Lailatul Sofia <lailatul.sofia24027@mhs.uingusdur.ac.id>
2026-09-22 06:39:53 +0700

    feat: buat tab navigation Beranda, Riwayat, Pengaturan

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+// import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+// import { AnimatedSplashOverlay } from '@/components/animated-icon';
+// import AppTabs from '@/components/app-tabs';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
+
+import { Tabs } from "expo-router"; 
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Tabs screenOptions={{ headerShown: true }}>
+      <Tabs.Screen name="index" options={{ title: "Beranda" }} />
+      <Tabs.Screen name="riwayat" options={{ title: "Riwayat" }} />
+      <Tabs.Screen name="pengaturan" options={{ title: "Pengaturan" }} />
+    </Tabs> 
   );
 }
