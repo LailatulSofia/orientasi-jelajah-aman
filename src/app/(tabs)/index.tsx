a9497693eb06330cee56a55a2e1dbddba407a0a0
Lailatul Sofia <lailatul.sofia24027@mhs.uingusdur.ac.id>
2026-09-22 06:23:35 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,9 +3,9 @@ import { View } from "react-native";
 import { useState, useEffect } from "react";
 // Catatan: gunakan ../components/ jika file berada di dalam folder app/
 // yang sejajar dengan folder components/
-import WeatherCard from "../../components/WeatherCard";
-import SearchBox from "../../components/SearchBox";
-import RiwayatList from "../../components/RiwayatList";
+import WeatherCard from "../../../components/WeatherCard";
+import SearchBox from "../../../components/SearchBox";
+import RiwayatList from "../../../components/RiwayatList";
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
@@ -24,7 +24,7 @@ export default function HalamanUtama() {
   }
 
   return (
-    <View style={{ padding: 16, gap: 16 }}>
+    <View style={{ padding: 16, paddingTop:80, gap: 16 }}>
       <SearchBox onCari={handleCari} />
       <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
       <RiwayatList daftarKota={riwayat} />
