0d04a77d93bead30fedc14b95fdf049813fe1cc2
Lailatul Sofia <lailatul.sofia24027@mhs.uingusdur.ac.id>
2026-09-22 09:34:32 +0700

    feat: tambah SafeAreaView dan responsive layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,6 @@
-// import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-// import * as SplashScreen from 'expo-splash-screen';
-// import { useColorScheme } from 'react-native';
-
-// import { AnimatedSplashOverlay } from '@/components/animated-icon';
-// import AppTabs from '@/components/app-tabs';
-
-// SplashScreen.preventAutoHideAsync();
-
 import { Tabs } from "expo-router"; 
 
 export default function TabLayout() {
-  // const colorScheme = useColorScheme();
   return (
     <Tabs screenOptions={{ headerShown: true }}>
       <Tabs.Screen name="index" options={{ title: "Beranda" }} />
